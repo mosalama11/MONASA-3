3dfbbd86444b8686135e06551b7ecfc99bbed749
mosalama11 <111012793+mosalama11@users.noreply.github.com>
2022-10-02 23:36:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// hamburger menu
+$('document').ready(function() {
+
+    $('.burger').click(function() {
+        $('.nav-menu').toggleClass("open");
+        $('.menu-list').toggleClass("list-open");
+    });
+
+});
+$('document').ready(function() {
+
+    $('.burger-link').click(function() {
+        $('.nav-menu').toggleClass("open");
+        $('.menu-list').toggleClass("list-open");
+    });
+
+});
